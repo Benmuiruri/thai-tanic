9a343b7e6cf70ccb31add2d28b46f6aec5fe0de0
Benmuiruri <ben.muiruri13@gmail.com>
2022-09-06 18:29:31 +0300

    Add useeffect to add a bump effect when the number of items in the header button change

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
 // @ts-nocheck
-import React, {useContext} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsActive, setBTnIsActive] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberofItems = cartCtx.items.reduce((currNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberofItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
-  }, 0 );
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsActive ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBTnIsActive(true);
+
+    const timer = setTimeout(() => {
+      setBTnIsActive(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
