e4eac6e405098b72d3c2ebd62321be61a9b87530
Benmuiruri <ben.muiruri13@gmail.com>
2022-09-06 09:03:30 +0300

    Use the cartContext to display no of Items in header

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
-import React from 'react';
+// @ts-nocheck
+import React, {useContext} from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberofItems = cartCtx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0 );
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={classes.badge}>4</span>
+      <span className={classes.badge}>{numberofItems}</span>
     </button>
   );
 };
