0ca8f3fc58cfa67b085f27dd0a9aabdd3e0dedae
Benmuiruri <ben.muiruri13@gmail.com>
2022-09-06 07:52:40 +0300

    Wrap App with CartProvider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsPresent, setCartIsPresent] = useState(false);
@@ -15,13 +16,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {cartIsPresent && <Cart onClose={hideCartHandler} /> }
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
