5f3118f4c6c4733f01a8600754ea149775a506c3
Benmuiruri <ben.muiruri13@gmail.com>
2022-09-06 09:36:31 +0300

    Add useRef to forward the input amount to the meal item Use useContext to add items to the cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,20 @@
+import React, { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
-import React from 'react';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+  const addItemToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      price: props.price,
+      amount: amount,
+    })
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +22,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div><MealItemForm id={props.id}/></div>
+      <div><MealItemForm id={props.id} onAddToCart={addItemToCartHandler}/></div>
     </li>
   );
 };
